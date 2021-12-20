71a3ac8f278fe3bc4f106c45b059f4b4520c7358
manu-tech-splique <56062589+emmanuelperletti@users.noreply.github.com>
2021-12-20 21:34:08 +0100

    final

--- log.py
+++ log.py
@@ -1,8 +1,15 @@
-from time import ctime
+from time import ctime, time
+
+
 def log(func):
+    """ fonction de log """
     def wrapper(*args, **kwargs):
-        with open("log.txt","a") as f:
-            f.write(f"{ctime()}:{func.__name__} called with params ({','.join(args)}) and {kwargs}\n")
-        val = func(*args,**kwargs)
+        t = time()
+        val = func(*args, **kwargs)
+        with open("log.txt", "a") as f:
+            f.write(
+                f"{ctime()}:{func.__module__+'.'+func.__name__} called with params ({','.join(args)}) and {kwargs} -> returned:{val} in {time()-t} seconds\n"
+            )
         return val
+
     return wrapper
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from log import log
 
 
 @log
-def func(chaine: str,*args,**kwargs) -> None:
-    print(f"La chaine {chaine}")   
+def func(param,*args,**kwargs) -> None:
+    """ fonction bateau, prenant des arguments et les retournant"""
+    print(f"Le parametre positionnel = '{param}', les arguments restant (*args) = {args} et les arguments nommés (**kwargs) = {kwargs}")
+    return (param,args,kwargs)   
 
 
 if __name__ == "__main__":
