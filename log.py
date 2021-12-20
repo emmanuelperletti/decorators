dc3c4a3d2e41c29e9c8ddbeabaa1b812a4edeb44
manu-tech-splique <56062589+emmanuelperletti@users.noreply.github.com>
2021-12-20 21:52:27 +0100

    corrected

--- log.py
+++ log.py
@@ -2,13 +2,13 @@ from time import ctime, time
 
 
 def log(func):
-    """ fonction de log """
+    """ logging decorator """
     def wrapper(*args, **kwargs):
         t = time()
         val = func(*args, **kwargs)
         with open("log.txt", "a") as f:
             f.write(
-                f"{ctime()}:{func.__module__+'.'+func.__name__} called with params ({','.join(args)}) and {kwargs} -> returned:{val} in {time()-t} seconds\n"
+                f"{ctime()}:{func.__module__+'.'+func.__name__} called with params {args} and {kwargs} -> returned:{val} in {time()-t} seconds\n"
             )
         return val
 
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from log import log
 
 @log
 def func(param,*args,**kwargs) -> None:
-    """ fonction bateau, prenant des arguments et les retournant"""
-    print(f"Le parametre positionnel = '{param}', les arguments restant (*args) = {args} et les arguments nommés (**kwargs) = {kwargs}")
+    """ dummy function printing parameters and returning them """
+    print(f"positionnal argument = '{param}', remaining arguments (*args) = {args} and keyword arguments (**kwargs) = {kwargs}")
     return (param,args,kwargs)   
 
 
 if __name__ == "__main__":
-    func("Hello", "test", name="toto")
+    func("positionnal_arg1", "remaining_arg1", arg1="named_arg1")
